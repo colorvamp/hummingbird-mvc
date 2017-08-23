bfdebb2841766f49f582edc3e1af3c9dc57065c5
Marcos <marcos.fernandez@mobusi.com>
2017-08-23 14:47:38 +0200

    cleanup

--- resources/controllers/index.php
+++ resources/controllers/index.php
@@ -3,44 +3,15 @@
 
 	function index_main($param = false){
 		global $TEMPLATE;
-		include_once('api.shoutbox.php');
 		include_once('inc.date.php');
 
-		if( isset($_POST['subcommand']) ){switch( $_POST['subcommand'] ){
-			case 'comment.add':
-				$shout = array(
-					'shoutText'=>$_POST['shoutText'],
-					'shoutAuthor'=>1 /* FIXME: hardcoded */
-				);
-				$r = shoutbox_save($shout);
-				if(isset($r['errorDescription'])){
-					print_r($r);
-					exit;
-				}
-				common_r();
-			default:
-				common_r();
-		}}
-
 		/* INI-Checking modes */
-		if(($d = dirname($GLOBALS['api']['shoutbox']['db'])) && ( !file_exists($d) || !is_writable($d) || !is_writable($GLOBALS['api']['shoutbox']['db']) )){
+		if( ($d = '../db/.') && ( !file_exists($d) || !is_writable($d) )){
 			$TEMPLATE['permission.warning'] = common_loadSnippet('snippets/permission.warning');
 		}
 		/* END-Checking modes */
 
-		/* INI-Painting the shouts */
-		if( class_exists('SQLite3') ){
-			$shoutOBs = shoutbox_getWhere('shoutResponseTo = 0');
-			$TEMPLATE['html.thread'] = '';
-			foreach($shoutOBs as $shoutOB){
-				$shoutOB['html.shoutDiff'] = date_humanReadable($shoutOB['shoutStamp']);
-				$TEMPLATE['html.thread'] .= common_loadSnippet('snippets/shout.node',$shoutOB);
-			}
-		}
-		/* END-Painting the shouts */
-
-
-		$TEMPLATE['PAGE.TITLE'] = 'colorvamp.com';
+		$TEMPLATE['PAGE.TITLE'] = 'Hummingbird';
 		return common_renderTemplate('index');
 	}
 
--- resources/views/index.php
+++ resources/views/index.php
@@ -1,35 +1,6 @@
 	<div class="wrapper">
 		{%permission.warning%}
-		{%html.thread%}
-		<div class="block">
-			<div class="image">
-				<div class="glow"></div>
-				<img src="{%w.indexURL%}/images/avatar"/>
-			</div>
-			<div class="wrapper">
-				<div class="block-header">
-					<ul class="tabs">
-						<li class="tab active"><span class="label">Write</span></li>
-						<li class="tab"><span class="label">Preview</span></li>
-						<li class="tab"><span class="label">Options</span></li>
-					</ul>
-				</div>
-				<div class="block-content">
-					<form method="post">
-						<input type="hidden" name="subcommand" value="comment.add">
-						<div class="textarea">
-							<textarea placeholder="Leave a comment" name="shoutText"></textarea>
-							<div class="textarea-footer">
-								Info
-							</div>
-						</div>
-
-						<div class="btn-group right">
-							<div class="btn disabled">Button disabled</div>
-							<button class="btn green">Comment</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		</div>
+		<section>
+			<h2>Welcome to Hummingbird</h2>
+		</section>
 	</div>
