73c5f3a37ec5bad042e487b72ae3781d16e0233a
sombra2eternity <sombra2eternity@gmail.com>
2017-09-19 16:53:03 +0200

    Create class._zip.php

---
+++ resources/libs/classes/class._zip.php
@@ -0,0 +1,74 @@
+<?php
+	class _zip{
+		public $zip  = false;
+		public $path = false;
+		public $file = false;
+		function __construct(...$args){
+			/* args variables -> (':rooms','17591','17-03-2015',true) */
+			$exists = false;
+			$last = array_pop($args);
+			if( is_bool($last) ){$exists = array_pop($args);}
+			if( substr($last,-4) !== '.zip' ){return false;}
+			if( !preg_match('![^/]+\.zip$!',$last,$_name) ){return false;}
+			$_name = $_name[0];
+
+			$last = substr($last,0, -1 * strlen($_name) );
+			if( $exists ){$args[] = $exists;}
+			$this->path = new _path(...$args);
+
+			$this->file = $this->path.$_name;
+		}
+		function __destruct(){
+			if( $this->zip ){$this->zip->close();}
+		}
+		function __toString(){
+			return $this->file;
+		}
+		function _open(){
+			if(  $this->zip ){return true;}
+			if( !$this->file ){return false;}
+			if( !($zip = new ZipArchive()) || $zip->open($this->file,ZipArchive::CREATE) !== true ){
+				if( file_exists($this->file) ){$r = unlink($this->file);}
+				return false;
+			}
+			$this->zip = $zip;
+			return true;
+		}
+		function stat(){
+			if( !file_exists($this->file) ){
+				return false;
+			}
+			$stat = stat($this->file);
+			$stat['diff'] = time() - $stat['mtime'];
+			$stat['diff.days'] = round( $stat['diff'] / 86400 );
+			return $stat;
+		}
+		function ls(){
+			if( !$this->_open() ){return ['errorDescription'=>'ZIP_ERROR','file'=>__FILE__,'line'=>__LINE__];}
+
+			$filenames = [];
+			for( $i = 0; $i < $this->zip->numFiles; $i++ ){
+				$filenames[] = $filename = $this->zip->getNameIndex($i);
+			}
+			return $filenames;
+		}
+		function blob($name = ''){
+			if( !$this->_open() ){return ['errorDescription'=>'ZIP_ERROR','file'=>__FILE__,'line'=>__LINE__];}
+
+			/* Devuelve 'false' si el fichero no existe */
+			$blob = $this->zip->getFromName($name);
+			return $blob;
+		}
+		function remove($path = ''){
+			if( !$this->_open() ){return ['errorDescription'=>'ZIP_ERROR','file'=>__FILE__,'line'=>__LINE__];}
+			return $this->zip->deleteName($path);
+		}
+		function file_put_contents($path = '',$blob = ''){
+			if( !$this->_open() ){return ['errorDescription'=>'ZIP_ERROR','file'=>__FILE__,'line'=>__LINE__];}
+			return $this->zip->addFromString($path,$blob);
+		}
+		function iterator($glob = '*',$callback = false,$params = []){
+			if( !$this->_open() ){return ['errorDescription'=>'ZIP_ERROR','file'=>__FILE__,'line'=>__LINE__];}
+			//FIXME: TODO
+		}
+	}
