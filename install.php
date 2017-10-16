ff55bd2b67e96467f6f880133a0ba398a99a1af7
Marcos <sombra2eternity@gmail.com>
2017-10-16 10:32:24 +0200

    Update install.php

--- install.php
+++ install.php
@@ -75,6 +75,8 @@
 					server_name mydomain.localhost;
 
 					location / {
+						# Redirect all to index
+						rewrite ^/(.*)$ /index.php last;
 						# First attempt to serve request as file, then
 						# as directory, then fall back to displaying a 404.
 						try_files $uri $uri/ =404;
