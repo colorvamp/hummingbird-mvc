21069617561ef88a80e3e3ddcc8e50ea93181a5c
Marcos <sombra2eternity@gmail.com>
2018-05-20 19:40:48 +0200

    Update class._git.php

--- resources/libs/classes/class._git.php
+++ resources/libs/classes/class._git.php
@@ -26,22 +26,23 @@
 		}
 		function _status(){
 			$data = shell_exec('cd "'.$this->path.'" && git status 2>&1');
-			if( preg_match('!fatal: This operation must be run in a work tree!',$data) ){
+			if (preg_match('!fatal: This operation must be run in a work tree!',$data)) {
 				return ['errorDescription'=>'INVALID_REPO','file'=>__FILE__,'line'=>__LINE__];
 			}
-			if( !preg_match('!On branch (?<branch>[^\n]+)!',$data) ){
+			if (!preg_match('!On branch (?<branch>[^\n]+)!',$data)) {
 				return ['errorDescription'=>'UNKNOWN_ERROR','file'=>__FILE__,'line'=>__LINE__];
 			}
 
 			$modified = [];
-			if( preg_match('!Changes not staged for commit:[ \n]*\(use[^\)]*\)[ \n]*\(use[^\)]*\)[ \n]*([\t ]*modified:[\t ]*(?<file>[^\n]+)\n)+!',$data,$m)
-			 && preg_match_all('![\t ]*modified:[\t ]*(?<file>[^\n]+)\n!',$m[0],$m) ){
+			//FIXME: maybe separate deleted from modified?
+			if (preg_match('!Changes not staged for commit:[ \n]*\(use[^\)]*\)[ \n]*\(use[^\)]*\)[ \n]*([\t ]*(modified|deleted):[\t ]*(?<file>[^\n]+)\n)+!',$data,$m)
+			 && preg_match_all('![\t ]*modified:[\t ]*(?<file>[^\n]+)\n!',$m[0],$m)) {
 				$modified = $m['file'];
 			}
 
 			$untracked = [];
-			if( preg_match('!Untracked files:[ \n]*\(use[^\)]*\)[ \n]*(?<body>([\t ]*(?<file>[^\n]+)\n)+)!',$data,$m)
-			 && preg_match_all('![\t ]*(?<file>[^\n]+)\n!',$m['body'],$m) ){
+			if (preg_match('!Untracked files:[ \n]*\(use[^\)]*\)[ \n]*(?<body>([\t ]*(?<file>[^\n]+)\n)+)!',$data,$m)
+			 && preg_match_all('![\t ]*(?<file>[^\n]+)\n!',$m['body'],$m)) {
 				$untracked = $m['file'];
 			}
 
