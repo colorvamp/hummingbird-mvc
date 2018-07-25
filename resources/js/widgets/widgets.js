43589020a4458e343ba2abdd18b338f4419a0893
Marcos <marcos@colorvamp.com>
2018-07-25 12:24:04 +0200

    Update widgets.js

--- resources/js/widgets/widgets.js
+++ resources/js/widgets/widgets.js
@@ -5,27 +5,32 @@
 	_widgets = function(){
 		this.selectors = {};
 
-		this._observer = new window.MutationObserver(function(mutations){
-			mutations.forEach(function(m,k){
-				if (!m.addedNodes.length) {return false;}
-				this.update();
+		addEventListener('DOMContentLoaded',function(){
+			this._observer = new window.MutationObserver(function(mutations){
+				mutations.forEach(function(m,k){
+					if (!m.addedNodes.length) {return false;}
+					this.update(m.target);
+				}.bind(this));
 			}.bind(this));
+			this._observer.observe(document.body, { childList:true, subtree:true, attributes:false});
+			
+			this.update(document.body);
 		}.bind(this));
-		this._observer.observe(document.body, { childList:true, subtree:true, attributes:false});
-
-		addEventListener('DOMContentLoaded',function(){this.update();}.bind(this));
 		if (document.readyState === 'complete' || document.readyState === 'loaded' || document.readyState === 'interactive') {
 			this.update();
 		}
 	};
-	_widgets.prototype.update = function(){
+	_widgets.prototype.update = function(target){
 		for (widget in this.selectors) {
-			var _nodes = Array.prototype.slice.call(m.target.querySelectorAll(this.selectors[widget]));
+			var _nodes = Array.prototype.slice.call(target.querySelectorAll(this.selectors[widget]));
 			_nodes.forEach(function(v, k) {new widgets[widget](v);});
 		}
 	};
 	_widgets.prototype.register = function(name,selector){
 		this.selectors[name] = selector;
+		/* First instance */
+		if (document.body) {
+			this.update(document.body);
+		}
 	};
 	window._widgets = new _widgets();
-
